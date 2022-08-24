8b87283510cc93fa14aa75d38fd2853410d8e275
izack8 <isaactaypb@gmail.com>
2022-08-24 15:06:18 +0800

    test line by iz

--- dip_commerce/lib/main.dart
+++ dip_commerce/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //Hello World, goodbye bitch
+        //Hello Wo草泥马
         // izack
         //rithikha
         //Tangxin
