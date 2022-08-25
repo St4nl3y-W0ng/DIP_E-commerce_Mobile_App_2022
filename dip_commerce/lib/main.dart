e801a7836a6d40c7c6f8f05e45a09dc3bbeb2d4a
Zhi Wei <53158793+jayzhiwei@users.noreply.github.com>
2022-08-25 11:35:59 +0800

    Texting ZW

--- dip_commerce/lib/main.dart
+++ dip_commerce/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Anyhow'),
+      home: const MyHomePage(title: 'Flutter Demo Home'),
     );
   }
 }
