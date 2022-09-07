d8c3f93896725dbbf6e1f116484ce19375809fd8
izack8 <isaactaypb@gmail.com>
2022-09-07 15:00:52 +0800

    firebase libs

--- back_end/lib/main.dart
+++ back_end/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 
 void main() {
   runApp(const MyApp());
