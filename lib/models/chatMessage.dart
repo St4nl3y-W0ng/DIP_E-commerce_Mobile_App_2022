c2e5b4b0b54b8e3243572608cb41d8df59ad5817
Stanley Wong <70022847+St4nl3y-W0ng@users.noreply.github.com>
2023-01-02 00:00:44 +0800

    Delete lib directory

--- lib/models/chatMessage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class chatMessage {
-  String chatId;
-  String message;
-  String time;
-
-  chatMessage({required this.chatId, required this.message, required this.time});
-}
